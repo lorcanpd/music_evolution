9de2cfa87d01e8d6dd63dbb9f9a3f871754f9b7a
Lorcan <lorcanpd@gmail.com>
2024-11-15 21:09:20 +0000

    Removed typo from genome.rs

--- src/genome.rs
+++ src/genome.rs
@@ -9,22 +9,22 @@ pub const MUTATION_RATE_BITS: usize = 8; // 8 bits for mutation rate
 pub const NOTE_START_BITS: usize = 8; // 8 bits for each note start time
 pub const GENOME_SIZE: usize = LEFT_CHROMOSOME_SIZE + RIGHT_CHROMOSOME_SIZE;
 pub const NOTE_CODON_SIZE: usize = 8; // Size of note codon in bits
-pub const EFFECT_CODON_SIZE: usize = 8; // Size of effect codon in bits
+pub const EFFECT_CODON_SIZE: usize = 9; // Size of effect codon in bits
 
 pub const NOTE_CODON: [u8; NOTE_CODON_SIZE] = [1, 1, 0, 1, 1, 1, 0, 0]; // Example pattern for note codon
 pub const SQUARE_WAVE_CODON: [u8; NOTE_CODON_SIZE] = [0, 1, 0, 1, 1, 1, 1, 1]; // Example pattern for square wave codon
 pub const CUSTOM_WAVE_CODON: [u8; NOTE_CODON_SIZE] = [1, 0, 1, 0, 0, 1, 1, 0]; // Example pattern for custom waveform codon
-pub const LOW_PASS_CODON: [u8; EFFECT_CODON_SIZE] = [1, 0, 1, 0, 1, 0, 1, 1];
-pub const HIGH_PASS_CODON: [u8; EFFECT_CODON_SIZE] = [0, 1, 0, 1, 0, 1, 0, 0];
-pub const REVERB_CODON: [u8; EFFECT_CODON_SIZE] = [1, 1, 1, 0, 1, 1, 1, 1];
-pub const ECHO_CODON: [u8; EFFECT_CODON_SIZE] = [0, 0, 0, 1, 0, 0, 0, 0];
+pub const LOW_PASS_CODON: [u8; EFFECT_CODON_SIZE] = [1, 0, 1, 0, 1, 0, 1, 1, 0];
+pub const HIGH_PASS_CODON: [u8; EFFECT_CODON_SIZE] = [0, 1, 0, 1, 0, 1, 0, 0, 0];
+pub const REVERB_CODON: [u8; EFFECT_CODON_SIZE] = [1, 1, 1, 0, 1, 1, 1, 1, 0];
+pub const ECHO_CODON: [u8; EFFECT_CODON_SIZE] = [0, 0, 0, 1, 0, 0, 0, 0, 0];
 
 pub struct Genome {
     left_chromosome: Vec<u8>,
     right_chromosome: Vec<u8>,
 }
 
-impl Genome {
+impl Chromosome {
     pub fn new(left_chromosome: Vec<u8>, right_chromosome: Vec<u8>) -> Self {
         Genome {
             left_chromosome,
@@ -42,8 +42,10 @@ impl Genome {
 
     pub fn initialise_random_genome() -> Self {
         let mut rng = rand::thread_rng();
-        let left_chromosome = (0..LEFT_CHROMOSOME_SIZE).map(|_| rng.gen_range(0..=1)).collect();
-        let right_chromosome = (0..RIGHT_CHROMOSOME_SIZE).map(|_| rng.gen_range(0..=1)).collect();
+        let left_chromosome = (
+            0..LEFT_CHROMOSOME_SIZE).map(|_| rng.gen_range(0..=1)).collect();
+        let right_chromosome = (
+            0..RIGHT_CHROMOSOME_SIZE).map(|_| rng.gen_range(0..=1)).collect();
         Genome {
             left_chromosome,
             right_chromosome,
